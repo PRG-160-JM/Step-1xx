09751bbf5d667a3f916bf671aa171040fbc8636e
jonathanmiller <penguinsj71@comcast.net>
2018-03-25 23:27:35 -0400

    added menuBuilder function to recursively build the menu and sub-menu items. We will complete the styling in the next branch.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 // JavaScript Document
 
+window.onload = init();
+
 function init() {
 
 	window.addEventListener('scroll', function (e) {
@@ -20,28 +22,51 @@ function init() {
 		url: 'assets/data/menu.json',
 		dataType: 'json',
 		success: function (data) {
-			console.log('all good');
-			console.log(data.menu.length);
-			console.log(data.menu);
 
-			if (data.menu.length > 0) {
-				data.menu.forEach(function (data) {
-					console.log(data.MenuName);
-					console.log(data.MenuLink);
+			var menu = menuBuilder(data.menu);
 
-					$('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
-				});
-			}
 		},
+
 		error: function () {
+
 			console.log('all is not good');
+
 		}
 
 	});
 
-
 }
 
+function menuBuilder(obj) {
 
+	var theMenu = '';
 
-window.onload = init();
+	if (obj.length > 0) {
+
+		theMenu = theMenu + '<ul>';
+
+		obj.forEach(function (item) {
+
+			theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+
+			if (item.Menus.length > 0) {
+
+				theMenu = theMenu + menuBuilder(item.Menus);
+
+			}
+
+			theMenu = theMenu + '</li>';
+
+		});
+
+		theMenu = theMenu + '</ul>';
+
+	} else {
+
+		console.log('no data');
+
+	}
+
+	return theMenu;
+
+}
