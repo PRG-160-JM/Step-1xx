cacb973837275db2eb8cc72401bac88f2029f85c
jonathanmiller <penguinsj71@comcast.net>
2018-04-08 21:40:22 -0400

    added click event functions and listeners. added preloader for home page on initial load. updated the links in the menu and logo to call page content from our WordPress API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// JavaScript Document
+//inside out project STEP-105
 
 window.onload = init();
 
@@ -25,13 +25,18 @@ function init() {
 		success: function (data) {
 
 			$("nav").hide();
-			
+
 			var menu = menuBuilder(data.items);
 
 			$('nav').html(menu).slideDown();
-			
-			$("#loaderDiv").fadeOut("slow");
 
+			$('nav li a').click(function () {
+				getPage($(this).data("pgid"));
+			});
+
+			getPage(86);
+
+			$("#loaderDiv").fadeOut("slow");
 		},
 		error: function () {
 
@@ -53,7 +58,9 @@ function menuBuilder(obj) {
 
 		obj.forEach(function (item) {
 
-			theMenu = theMenu + '<li><a href="#">' + item.title + '</a>';
+			theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
+
+
 
 			if (item.children) {
 
@@ -74,4 +81,31 @@ function menuBuilder(obj) {
 	}
 
 	return theMenu;
+}
+
+function getPage(obj) {
+	$("#loaderDiv").fadeIn("slow");
+	$.ajax({
+		method: 'GET',
+		url: 'https://me.iambeardednbroke.com/wp-json/wp/v2/pages/' + obj,
+		datatype: 'json',
+		success: function (data) {
+			var pgbuild = '';
+			pgbuild = '<section><div class ="container">' + data.content.rendered + '</div></section>';
+			$("#content").fadeOut(function () {
+				$('html').animate({
+					scrollTop: 0
+				}, 'slow');
+				$('body').animate({
+					scrollTop: 0
+				}, 'slow');
+				$(this).html(pgbuild).fadeIn();
+				$("#loaderDiv").fadeOut("slow");
+
+			});
+		},
+		error: function () {
+			console.log('bad');
+		}
+	});
 }
